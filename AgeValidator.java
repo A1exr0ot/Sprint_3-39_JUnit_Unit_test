74963132aa3b1363531dddce382cfa2fc50baf42
Alex <85393489+A1exr0ot@users.noreply.github.com>
2026-01-02 20:54:03 +0500

    Delete AgeValidator.java

--- AgeValidator.java
+++
@@ -1,8 +0,0 @@
-public class AgeValidator {
-    public static final int MIN_AGE = 18;
-    public static final int MAX_AGE = 65;
-
-    public boolean isValidAge(int age) {
-        return age >= MIN_AGE && age <= MAX_AGE;
-    }
-}
